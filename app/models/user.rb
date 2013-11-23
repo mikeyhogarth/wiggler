962e4c78a96515461ce5e88110fbbb04a3e5eef7
mikeyhogarth <mikehogarth20@hotmail.com>
2013-11-23 12:43:51 +0000

    Preparing for first live deploy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  #devise :database_authenticatable, :registerable,:recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :rememberable
 
   has_many :opinions
 
-  validates_presence_of :first_name
-  validates_presence_of :last_name
+  #validates_presence_of :first_name
+  #validates_presence_of :last_name
 
   def full_name
     "#{first_name} #{last_name}"
