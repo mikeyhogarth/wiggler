499c7db0bd2ab7f470cd20b30beeaec951343e30
Mikey Hogarth <mikehogarth20@hotmail.com>
2013-12-08 12:55:14 +0000

    Making top nav work, additional changes to sample data generator

--- lib/generators/sample_data/sample_data_generator.rb
+++ lib/generators/sample_data/sample_data_generator.rb
@@ -4,22 +4,31 @@ class SampleDataGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
 
   LOREM_IPSUM = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' 
- 
 
+  #
+  # Create sample wiggles from yml file 
+  #
   def populate_wiggles
-    puts "Destroying previous data..."
-    Wiggle.destroy_all    
-
-    puts "Loading new sample data..."
+    return if Wiggle.exists?
+    print "Creating Wiggles..."
     sample = YAML.load_file(File.expand_path("lib/generators/sample_data/wiggles.yml"))
 
-    print "Creating Wiggles..."
     sample["wiggles"].each do |wiggle| 
       print "."
       Wiggle.create(:name => wiggle["name"].chomp, :description => LOREM_IPSUM)
     end
-
-    puts "\nSample data generation complete."
   end
+
+
+  #
+  # Create a sample test user
+  #
+  def create_user_if_none_exist
+    return if User.exists?
+    puts "Creating Users..."
+
+    User.create :email => "test@test.com", :password => "welcome1", :password_confirmation => "welcome1"
+  end
+
 
 end
